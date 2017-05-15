750a22e36b4c4fa22609196f18728d0a406bd7e5
zhicheng <zhichengsun@deepglint.com>
2017-05-15 14:40:29 +0800

    change vimrc and add vim setup script

---
+++ vim_setup.sh
@@ -0,0 +1,11 @@
+
+# install vim plugins
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp .vimrc ~
+vim +PluginInstall +qall
+
+# install ycm
+sudo apt-get install build-essential cmake
+sudo apt-get install python-dev python3-dev
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer
