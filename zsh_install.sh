0efcefc69eb42acea2bf8c5b02fc8ec61deb7a40
zhichengsun <zhch.sun@gmail.com>
2017-06-08 07:23:52 -0400

    revise zsh install script

--- zsh_install.sh
+++ zsh_install.sh
@@ -4,10 +4,11 @@ sudo add-apt-repository ppa:aacebedo/fasd
 sudo apt-get update
 sudo apt-get install fasd
 
+sudo apt-get -y install zsh
 # install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # install tmux
-sudo apt-get install tmux
+# sudo apt-get install tmux
 
-source ~/.zshrc
+# source ~/.zshrc
